2ffa90ee1b795b91a3158d9e90ef0b0fbd57dea1
mansoorameen <manzooravail@gmail.com>
2021-04-17 12:54:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ useEffect(() => {
   db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
     setTodos(snapshot.docs.map(doc => ({id: doc.id ,todo: doc.data().todo})))
   } ) 
-  // eslint-disabled-next-line
+  
 }, []);
 
 const addTodo = (event) => {
